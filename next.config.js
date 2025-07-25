34f3317ee980c751d55abdc15e79b0fe80d115b6
MC - Marcos <admin@mabelcode.com.br>
2025-07-25 19:36:22 -0300

    .

--- next.config.js
+++ next.config.js
@@ -1,16 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  experimental: {
+    outputFileTracingIgnores: ['**/.next/cache/**'],
   },
   images: {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [320, 420, 768, 1024, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    minimumCacheTTL: 604800, // 1 semana
-    // domains: [], // Se precisar liberar domínios externos no futuro
+    minimumCacheTTL: 604800,
     unoptimized: false,
   },
 };
-
 module.exports = nextConfig;
